07964275feda511cd7f851218f83671fe76cfe37
RobertMPuiu <44621876+RobertMPuiu@users.noreply.github.com>
2020-06-18 02:02:58 +0200

    Add files via upload

--- Proyecto/Views/index.php
+++ Proyecto/Views/index.php
@@ -6,7 +6,7 @@ session_start();
 	<head>
 		<meta charset="utf-8">
 		<meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no">
-		<title>Inicio</title>
+		<title>FilmZona</title>
 		<!-- CDN jquery -->
 		<script src="http://ajax.googleapis.com/ajax/libs/jquery/1.7.2/jquery.min.js"></script>
 		<!-- script para el menu -->
